0514a3045fabb96256ae57d61e4c83a665ef0f24
wcjohnson11 <williamcouperjohnson@gmail.com>
2016-04-21 07:45:38 -0700

    basic identify test failing

--- src/main/java/com/segment/analytics/android/integrations/taplytics/TaplyticsIntegration.java
+++ src/main/java/com/segment/analytics/android/integrations/taplytics/TaplyticsIntegration.java
@@ -3,47 +3,67 @@ package com.segment.analytics.android.integrations.taplytics;
 import com.segment.analytics.Analytics;
 import com.segment.analytics.Properties;
 import com.segment.analytics.ValueMap;
+import com.segment.analytics.integrations.IdentifyPayload;
 import com.segment.analytics.integrations.Integration;
 import com.segment.analytics.integrations.Logger;
 import com.segment.analytics.integrations.TrackPayload;
 import com.taplytics.sdk.Taplytics;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.json.JSONObject;
+
+import static com.segment.analytics.internal.Utils.transform;
 
 /**
  * Created by williamjohnson on 4/19/16.
  */
 public class TaplyticsIntegration extends Integration<Taplytics> {
-    public static final Factory FACTORY = new Factory() {
-        @Override public Integration<?> create(ValueMap settings, Analytics analytics) {
-            return new TaplyticsIntegration(analytics, settings);
-        }
-
-        @Override public String key() {
-            return TAPLYTICS_KEY;
-        }
-    };
-    private static final String TAPLYTICS_KEY = "Taplytics";
-    final Logger logger;
-    String apiKey;
-    boolean liveUpdate;
-    int sessionMinutes;
-
-    TaplyticsIntegration(Analytics analytics, ValueMap settings){
-      logger = analytics.logger(TAPLYTICS_KEY);
-
-      String apiKey = settings.getString("apiKey");
-      liveUpdate = settings.getBoolean("liveUpdate", true);
-      sessionMinutes = settings.getInt("sessionMinutes", 10);
-
-      Taplytics.startTaplytics(analytics.getApplication(), apiKey);
-      logger.verbose("Taplytics.startTaplytics(analytics.getApplication(), %s)", apiKey);
+  public static final Factory FACTORY = new Factory() {
+    @Override public Integration<?> create(ValueMap settings, Analytics analytics) {
+      return new TaplyticsIntegration(analytics, settings);
     }
-
-    @Override public void track(TrackPayload track) {
-      String event = track.event();
-
-      event(event, track.properties());
+    @Override public String key() {
+      return TAPLYTICS_KEY;
     }
+  };
+
+  private static final String TAPLYTICS_KEY = "Taplytics";
+
+  static final Map<String, String> MAPPER;
+
+  static {
+    Map<String, String> mapper = new LinkedHashMap<>();
+    mapper.put("firstName", "firstname");
+    mapper.put("lastName", "lastname");
+    mapper.put("userId", "user_id");
+    MAPPER = Collections.unmodifiableMap(mapper);
+  }
+
+  final Logger logger;
+  String apiKey;
+  boolean liveUpdate;
+  int sessionMinutes;
+
+  TaplyticsIntegration(Analytics analytics, ValueMap settings){
+    logger = analytics.logger(TAPLYTICS_KEY);
+    String apiKey = settings.getString("apiKey");
+    liveUpdate = settings.getBoolean("liveUpdate", true);
+    sessionMinutes = settings.getInt("sessionMinutes", 10);
+    Taplytics.startTaplytics(analytics.getApplication(), apiKey);
+    logger.verbose("Taplytics.startTaplytics(analytics.getApplication(), %s)", apiKey);
+  }
+
+  @Override public void track(TrackPayload track) {
+    String event = track.event();
+    event(event, track.properties());
+  }
+
+  @Override public void identify(IdentifyPayload identify) {
+    super.identify(identify);
+    JSONObject traits = new ValueMap(transform(identify.traits(), MAPPER)).toJsonObject();
+    Taplytics.setUserAttributes(traits);
+  }
 
   private void event(String name, Properties properties) {
     JSONObject propertiesJSON = properties.toJsonObject();
--- src/test/java/com/segment/analytics/android/integrations/taplytics/TaplyticsTest.java
+++ src/test/java/com/segment/analytics/android/integrations/taplytics/TaplyticsTest.java
@@ -5,14 +5,17 @@ import android.app.Application;
 import android.os.Bundle;
 import com.segment.analytics.Analytics;
 import com.segment.analytics.Properties;
+import com.segment.analytics.Traits;
 import com.segment.analytics.ValueMap;
 import com.segment.analytics.integrations.Logger;
 import com.segment.analytics.integrations.TrackPayload;
+import com.segment.analytics.test.IdentifyPayloadBuilder;
 import com.segment.analytics.test.TrackPayloadBuilder;
 import com.taplytics.sdk.Taplytics;
 
 import org.hamcrest.Description;
 import org.hamcrest.TypeSafeMatcher;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Rule;
@@ -45,95 +48,100 @@ import static org.powermock.api.mockito.PowerMockito.when;
 public class TaplyticsTest {
 
   @Rule
-    public PowerMockRule rule = new PowerMockRule();
-    @Mock
-    Application context;
-    Logger logger;
-    @Mock
-    Analytics analytics;
-    TaplyticsIntegration integration;
-    @Mock Taplytics taplytics;
+  public PowerMockRule rule = new PowerMockRule();
+  @Mock
+  Application context;
+  Logger logger;
+  @Mock
+  Analytics analytics;
+  TaplyticsIntegration integration;
+  @Mock Taplytics taplytics;
 
-    @Before
-    public void setUp() {
-        initMocks(this);
-        mockStatic(Taplytics.class);
-        logger = Logger.with(Analytics.LogLevel.DEBUG);
-        when(analytics.logger("Taplytics")).thenReturn(logger);
-        when(analytics.getApplication()).thenReturn(context);
+  @Before
+  public void setUp() {
+    initMocks(this);
+    mockStatic(Taplytics.class);
+    logger = Logger.with(Analytics.LogLevel.DEBUG);
+    when(analytics.logger("Taplytics")).thenReturn(logger);
+    when(analytics.getApplication()).thenReturn(context);
+    integration = new TaplyticsIntegration(analytics, new ValueMap().putValue("apiKey", "foo"));
+  }
 
-        integration = new TaplyticsIntegration(analytics, new ValueMap().putValue("apiKey", "foo"));
-    }
+  @Test
+  public void factory() {
+    ValueMap settings = new ValueMap() //
+        .putValue("apiKey", "foo")
+        .putValue("liveUpdate", false)
+        .putValue("sessionMinutes", 20);
+    TaplyticsIntegration integration =
+        (TaplyticsIntegration) TaplyticsIntegration.FACTORY.create(settings, analytics);
+    verifyStatic();
+    //Integration initialized
+    //Make sure settings are set correctly
+    assertThat(integration.liveUpdate).isFalse();
+    assertThat(integration.sessionMinutes).isEqualTo(20);
+  }
 
-    @Test
-    public void factory() {
-        ValueMap settings = new ValueMap() //
-            .putValue("apiKey", "foo")
-            .putValue("liveUpdate", false)
-            .putValue("sessionMinutes", 20);
+  @Test
+  public void initializeWithDefaultArguments() {
+    ValueMap settings = new ValueMap() //
+        .putValue("apiKey", "foo");
+    TaplyticsIntegration integration =
+        (TaplyticsIntegration) TaplyticsIntegration.FACTORY.create(settings, analytics);
+    verifyStatic();
+    //Integration initialized
+    //Make sure settings are set correctly
+    assertThat(integration.liveUpdate).isTrue();
+    assertThat(integration.sessionMinutes).isEqualTo(10);
+  }
 
-        TaplyticsIntegration integration =
-                (TaplyticsIntegration) TaplyticsIntegration.FACTORY.create(settings, analytics);
+  @Test public void track() {
+    integration.track(new TrackPayloadBuilder().event("foo").build());
+    verify(taplytics).logEvent(eq("foo"), eq(0.0), jsonEq(new JSONObject()));
+  }
 
-        verifyStatic();
-        //Integration initialized
-        //Make sure settings are set correctly
-        assertThat(integration.liveUpdate).isFalse();
-        assertThat(integration.sessionMinutes).isEqualTo(20);
-    }
+  @Test public void trackWithValue() {
+    integration.track(new TrackPayloadBuilder().event("foo")
+        .properties(new Properties().putValue(20.0))
+        .build());
+    verify(taplytics).logEvent(eq("foo"), eq(20.0), jsonEq(new JSONObject()));
+  }
 
-    @Test
-    public void initializeWithDefaultArguments() {
-      ValueMap settings = new ValueMap() //
-          .putValue("apiKey", "foo");
+  @Test public void identify() throws JSONException {
+    Traits traits = createTraits("foo");
+    integration.identify(new IdentifyPayloadBuilder().traits(traits).build());
 
-      TaplyticsIntegration integration =
-          (TaplyticsIntegration) TaplyticsIntegration.FACTORY.create(settings, analytics);
+    JSONObject attributes = new JSONObject();
+    attributes.put("user_id", "foo");
 
-      verifyStatic();
-      //Integration initialized
-      //Make sure settings are set correctly
-      assertThat(integration.liveUpdate).isTrue();
-      assertThat(integration.sessionMinutes).isEqualTo(10);
-    }
+    verifyStatic();
+    verify(taplytics).setUserAttributes(attributes);
 
-    @Test public void track() {
-      integration.track(new TrackPayloadBuilder().event("foo").build());
+  }
 
-      verify(taplytics).logEvent(eq("foo"), eq(0.0), jsonEq(new JSONObject()));
-    }
-
-    @Test public void trackWithValue() {
-      integration.track(new TrackPayloadBuilder().event("foo")
-          .properties(new Properties().putValue(20.0))
-          .build());
-
-      verify(taplytics).logEvent(eq("foo"), eq(20.0), jsonEq(new JSONObject()));
-    }
 
   private void verifyNoMoreTaplyticsInteractions() {
       verifyNoMoreInteractions(Taplytics.class);
       verifyNoMoreInteractions(taplytics);
     }
 
-    public static JSONObject jsonEq(JSONObject expected) {
-      return argThat(new JSONObjectMatcher(expected));
+  public static JSONObject jsonEq(JSONObject expected) {
+    return argThat(new JSONObjectMatcher(expected));
+  }
+
+  private static class JSONObjectMatcher extends TypeSafeMatcher<JSONObject> {
+    private final JSONObject expected;
+    private JSONObjectMatcher(JSONObject expected) {
+      this.expected = expected;
     }
 
-    private static class JSONObjectMatcher extends TypeSafeMatcher<JSONObject> {
-      private final JSONObject expected;
-
-      private JSONObjectMatcher(JSONObject expected) {
-        this.expected = expected;
-      }
-
-      @Override public boolean matchesSafely(JSONObject jsonObject) {
-        // todo: this relies on having the same order
-        return expected.toString().equals(jsonObject.toString());
-      }
-
-      @Override public void describeTo(Description description) {
-        description.appendText(expected.toString());
-      }
+    @Override public boolean matchesSafely(JSONObject jsonObject) {
+      // todo: this relies on having the same order
+      return expected.toString().equals(jsonObject.toString());
     }
+
+    @Override public void describeTo(Description description) {
+      description.appendText(expected.toString());
+    }
+  }
 }
