e3acca6be1e289caa040576a1359c53da011b8f6
vicv <victorvuc@gmail.com>
2016-10-03 17:24:54 -0400

    Expect v2

--- src/main/java/com/segment/analytics/android/integrations/taplytics/TaplyticsIntegration.java
+++ src/main/java/com/segment/analytics/android/integrations/taplytics/TaplyticsIntegration.java
@@ -8,10 +8,12 @@ import com.segment.analytics.integrations.Integration;
 import com.segment.analytics.integrations.Logger;
 import com.segment.analytics.integrations.TrackPayload;
 import com.taplytics.sdk.Taplytics;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+
 import org.json.JSONObject;
 
 import static com.segment.analytics.internal.Utils.transform;
@@ -20,92 +22,95 @@ import static com.segment.analytics.internal.Utils.transform;
  * Created by williamjohnson on 4/19/16.
  */
 public class TaplyticsIntegration extends Integration<Taplytics> {
-  public static final Factory FACTORY = new Factory() {
-    @Override
-    public Integration<?> create(ValueMap settings, Analytics analytics) {
-      return new TaplyticsIntegration(analytics, settings);
+    public static final Factory FACTORY = new Factory() {
+        @Override
+        public Integration<?> create(ValueMap settings, Analytics analytics) {
+            return new TaplyticsIntegration(analytics, settings);
+        }
+
+        @Override
+        public String key() {
+            return TAPLYTICS_KEY;
+        }
+    };
+
+    private static final String TAPLYTICS_KEY = "Taplytics";
+    private static final String TAPLYTICS_KEY_API_KEY = "apiKey";
+    private static final String TAPLYTICS_OPTION_LIVE_UPDATE = "liveUpdate";
+    private static final String TAPLYTICS_OPTION_SHAKE_MENU = "shakeMenu";
+    private static final String TAPLYTICS_OPTION_TURN_MENU = "turnMenu";
+    private static final String TAPLYTICS_OPTION_LIVE_UPDATE_V2 = "liveUpdateV2";
+    private static final String TAPLYTICS_OPTION_SHAKE_MENU_V2 = "shakeMenuV2";
+    private static final String TAPLYTICS_OPTION_TURN_MENU_V2 = "turnMenuV2";
+    private static final String TAPLYTICS_OPTION_SESSION_MINUTES = "sessionMinutes";
+    private static final String TAPLYTICS_OPTION_DELAYED_START = "delayedStartTaplytics";
+
+    private static final Map<String, String> MAPPER;
+
+    static {
+        Map<String, String> mapper = new LinkedHashMap<>();
+        mapper.put("firstName", "firstname");
+        mapper.put("lastName", "lastname");
+        mapper.put("userId", "user_id");
+        MAPPER = Collections.unmodifiableMap(mapper);
+    }
+
+    private enum TaplyticsValue {
+        OFF(false), ON(true), DEFAULT(null);
+
+        private Boolean value = null;
+
+        TaplyticsValue(Boolean val) {
+            this.value = val;
+        }
+
+        public Boolean getValue() {
+            return value;
+        }
+    }
+
+    private final Logger logger;
+
+    TaplyticsIntegration(Analytics analytics, ValueMap settings) {
+        logger = analytics.logger(TAPLYTICS_KEY);
+        String apiKey = settings.getString(TAPLYTICS_KEY_API_KEY);
+        int liveUpdate = settings.getInt(TAPLYTICS_OPTION_LIVE_UPDATE_V2, TaplyticsValue.DEFAULT.ordinal());
+        int shakeMenu = settings.getInt(TAPLYTICS_OPTION_SHAKE_MENU_V2, TaplyticsValue.DEFAULT.ordinal());
+        int turnMenu = settings.getInt(TAPLYTICS_OPTION_TURN_MENU_V2, TaplyticsValue.DEFAULT.ordinal());
+        int sessionMinutes = settings.getInt(TAPLYTICS_OPTION_SESSION_MINUTES, 10);
+        HashMap<String, Object> options = new HashMap<>();
+        options.put(TAPLYTICS_OPTION_LIVE_UPDATE, TaplyticsValue.values()[liveUpdate].value);
+        options.put(TAPLYTICS_OPTION_SHAKE_MENU, TaplyticsValue.values()[shakeMenu].value);
+        options.put(TAPLYTICS_OPTION_TURN_MENU, TaplyticsValue.values()[turnMenu].value);
+        options.put(TAPLYTICS_OPTION_SESSION_MINUTES, sessionMinutes);
+        options.put(TAPLYTICS_OPTION_DELAYED_START, true);
+        Taplytics.startTaplytics(analytics.getApplication(), apiKey, options);
+        logger.verbose("Taplytics.startTaplytics(analytics.getApplication(), %s, %s)", apiKey, options);
     }
 
     @Override
-    public String key() {
-      return TAPLYTICS_KEY;
-    }
-  };
-
-  private static final String TAPLYTICS_KEY = "Taplytics";
-  private static final String TAPLYTICS_KEY_API_KEY = "apiKey";
-  private static final String TAPLYTICS_OPTION_LIVE_UPDATE = "liveUpdate";
-  private static final String TAPLYTICS_OPTION_SHAKE_MENU = "shakeMenu";
-  private static final String TAPLYTICS_OPTION_TURN_MENU = "turnMenu";
-  private static final String TAPLYTICS_OPTION_SESSION_MINUTES = "sessionMinutes";
-  private static final String TAPLYTICS_OPTION_DELAYED_START = "delayedStartTaplytics";
-
-  private static final Map<String, String> MAPPER;
-
-  static {
-    Map<String, String> mapper = new LinkedHashMap<>();
-    mapper.put("firstName", "firstname");
-    mapper.put("lastName", "lastname");
-    mapper.put("userId", "user_id");
-    MAPPER = Collections.unmodifiableMap(mapper);
-  }
-
-  private enum TaplyticsValue {
-    OFF(false), ON(true), DEFAULT(null);
-
-    private Boolean value = null;
-
-    TaplyticsValue(Boolean val) {
-      this.value = val;
+    public void track(TrackPayload track) {
+        String event = track.event();
+        event(event, track.properties());
     }
 
-    public Boolean getValue() {
-      return value;
+    @Override
+    public void identify(IdentifyPayload identify) {
+        super.identify(identify);
+        JSONObject traits = new ValueMap(transform(identify.traits(), MAPPER)).toJsonObject();
+        Taplytics.setUserAttributes(traits);
+        logger.verbose("Taplytics.setUserAttributes(%s)", traits);
     }
-  }
 
-  private final Logger logger;
-
-  TaplyticsIntegration(Analytics analytics, ValueMap settings) {
-    logger = analytics.logger(TAPLYTICS_KEY);
-    String apiKey = settings.getString(TAPLYTICS_KEY_API_KEY);
-    int liveUpdate = settings.getInt(TAPLYTICS_OPTION_LIVE_UPDATE, TaplyticsValue.DEFAULT.ordinal());
-    int shakeMenu = settings.getInt(TAPLYTICS_OPTION_SHAKE_MENU, TaplyticsValue.DEFAULT.ordinal());
-    int turnMenu = settings.getInt(TAPLYTICS_OPTION_TURN_MENU, TaplyticsValue.DEFAULT.ordinal());
-    int sessionMinutes = settings.getInt(TAPLYTICS_OPTION_SESSION_MINUTES, 10);
-    HashMap<String, Object> options = new HashMap<>();
-    options.put(TAPLYTICS_OPTION_LIVE_UPDATE, TaplyticsValue.values()[liveUpdate].value);
-    options.put(TAPLYTICS_OPTION_SHAKE_MENU, TaplyticsValue.values()[shakeMenu].value);
-    options.put(TAPLYTICS_OPTION_TURN_MENU, TaplyticsValue.values()[turnMenu].value);
-    options.put(TAPLYTICS_OPTION_SESSION_MINUTES, sessionMinutes);
-    options.put(TAPLYTICS_OPTION_DELAYED_START, true);
-    Taplytics.startTaplytics(analytics.getApplication(), apiKey, options);
-    logger.verbose("Taplytics.startTaplytics(analytics.getApplication(), %s, %s)", apiKey, options);
-  }
-
-  @Override
-  public void track(TrackPayload track) {
-    String event = track.event();
-    event(event, track.properties());
-  }
-
-  @Override
-  public void identify(IdentifyPayload identify) {
-    super.identify(identify);
-    JSONObject traits = new ValueMap(transform(identify.traits(), MAPPER)).toJsonObject();
-    Taplytics.setUserAttributes(traits);
-    logger.verbose("Taplytics.setUserAttributes(%s)", traits);
-  }
-
-  private void event(String name, Properties properties) {
-    JSONObject propertiesJSON = properties.toJsonObject();
-    int revenue = (int) properties.revenue();
-    if (revenue != 0) {
-      Taplytics.logRevenue(name, revenue, propertiesJSON);
-      logger.verbose("Taplytics.logRevenue(%s, %s, %s)", name, revenue, propertiesJSON);
-      return;
+    private void event(String name, Properties properties) {
+        JSONObject propertiesJSON = properties.toJsonObject();
+        int revenue = (int) properties.revenue();
+        if (revenue != 0) {
+            Taplytics.logRevenue(name, revenue, propertiesJSON);
+            logger.verbose("Taplytics.logRevenue(%s, %s, %s)", name, revenue, propertiesJSON);
+            return;
+        }
+        Taplytics.logEvent(name, properties.value(), propertiesJSON);
+        logger.verbose("Taplytics.logEvent(%s, %s, %s)", name, properties.value(), propertiesJSON);
     }
-    Taplytics.logEvent(name, properties.value(), propertiesJSON);
-    logger.verbose("Taplytics.logEvent(%s, %s, %s)", name, properties.value(), propertiesJSON);
-  }
 };
